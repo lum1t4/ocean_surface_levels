de1fed240ca53d80965c235ff21fb488fde9f4b9
Gianluca Calò <gianluca.calo11@gmail.com>
2025-04-08 16:19:11 +0200

    added initial dvc data pipeline

---
+++ scripts/aggregate.py
@@ -0,0 +1,29 @@
+import xarray as xr
+from pathlib import Path
+import argparse
+
+
+def aggregate(src: str, dst: str):
+    src = Path(src)
+    dst = Path(dst)
+
+    assert src.is_dir(), f"Source path {src} is not a directory or does not exist"
+    files = list(src.glob('**/*.nc'))
+    # Open multiple files as a single dataset
+    dataset = xr.open_mfdataset(
+        files,
+        combine='by_coords',  # Combines along matching dimensions (e.g., time, lat, lon)
+        parallel=False        # Disables parallel reading to avoid segmentation fault
+    )
+    # Load the dataset into memory (optional, but useful for faster access)
+    dataset = dataset.load()
+    # Save the dataset to a single NetCDF file
+    dataset.to_netcdf(dst)
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Aggregate data by coords")
+    parser.add_argument("--src", type=str, help="Dataset to download")
+    parser.add_argument("--dst", type=str, help="filename", default="data/processed")
+    arguments = parser.parse_args()
+    aggregate(arguments.src, arguments.dst)
--- scripts/download.py
+++ scripts/download.py
@@ -3,17 +3,21 @@ import os
 import argparse
 from pathlib import Path
 
-USERNAME = os.getenv("CMEMS_USERNAME")
-PASSWORD = os.getenv("CMEMS_PASSWORD")
+
+
+def download(src: str, dst: str):
+    USERNAME = os.getenv("CMEMS_USERNAME")
+    PASSWORD = os.getenv("CMEMS_PASSWORD")
+
+    assert USERNAME is not None, "CMEMS_USERNAME environment variable is not set"
+    assert PASSWORD is not None, "CMEMS_PASSWORD environment variable is not set"
+    assert Path(dst).exists(), f"Output directory {dst} does not exist"
+    copernicusmarine.get(src, username=USERNAME, password=PASSWORD, output_directory=dst)
+
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Download sea surface heights data")
-    # cmems_obs-sl_eur_phy-ssh_nrt_allsat-l4-duacs-0.125deg_P1D
-    parser.add_argument("--dataset", type=str, help="Dataset to download", default="cmems_obs-sl_eur_phy-ssh_nrt_allsat-l4-duacs-0.125deg_P1D")
-    parser.add_argument("--output_directory", type=str, help="Output directory", default="data/raw")
+    parser.add_argument("--src", type=str, help="Dataset to download", default="cmems_obs-sl_eur_phy-ssh_nrt_allsat-l4-duacs-0.125deg_P1D")
+    parser.add_argument("--dst", type=str, help="Destination directory", default="data/raw")
     args = parser.parse_args()
-
-    dataset = args.dataset
-    output_directory = args.output_directory
-    assert Path(output_directory).exists(), f"Output directory {output_directory} does not exist"
-    copernicusmarine.get(dataset, username=USERNAME, password=PASSWORD, output_directory=output_directory)
+    download(args.src, args.dst)
